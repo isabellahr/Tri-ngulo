1d00a3aca8849f2845c2d610cc69f2f741aa8c82
isabellahr <rachwal.isabella@escola.pr.gov.br>
2024-10-18 08:51:57 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500); // criar cenário
+  background("lightblue"); // fundo azul
+}
+
+function draw() {
+fill ("purple")  // preencher de roxo
+stroke("red"); // vermelho linha
+
+triangle(100, 300, 200, 100, 300, 300); // coorenadas triangluo
+}
